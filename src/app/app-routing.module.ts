0767cceb0a11aafe5f496608d50cf2b6aa037aea
maxestad <contact@maxestad.com>
2023-06-13 21:03:57 +0200

    chore: initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import type { Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
